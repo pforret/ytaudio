2ec2a6e2e951aefa6ed90f523fa3d890c860b164
Peter Forret <peter@forret.com>
2023-12-31 15:21:46 +0100

    MOD: test_download.sh

--- tests/test_download.sh
+++ tests/test_download.sh
@@ -25,7 +25,7 @@ fi
 test_download_youtube() {
   # script without parameters should give usage info
   local output_file
-  output_file="$("$root_script" --OUT_DIR ../temp --SPLITTER "" get "https://www.youtube.com/watch?v=XEjLoHdbVeE")"
+  output_file="$("$root_script" --FORMAT wav --OUT_DIR ../temp --SPLITTER "" get "https://www.youtube.com/watch?v=XEjLoHdbVeE")"
   assert_equals "../temp/ABBA_-_Gimme_Gimme_Gimme_A_Man_After_Midnight.210s.wav" "$output_file"
   assert_equals 40235618 "$(< "$output_file" wc -c | awk '{print $1}')"
 }
@@ -33,7 +33,7 @@ test_download_youtube() {
 test_download_soundcloud() {
   # script without parameters should give usage info
   local output_file
-  output_file="$("$root_script" --OUT_DIR ../temp --SPLITTER "" get "https://soundcloud.com/kaytranada/janet-jackson-if-kaytranada")"
+  output_file="$("$root_script" --FORMAT wav --OUT_DIR ../temp --SPLITTER "" get "https://soundcloud.com/kaytranada/janet-jackson-if-kaytranada")"
   assert_equals "../temp/Janet_Jackson_-_If_Kaytranada_Remix.237.819s.wav" "$output_file"
   assert_equals 41946794 "$(< "$output_file" wc -c | awk '{print $1}')"
 }
@@ -41,7 +41,7 @@ test_download_soundcloud() {
 test_download_mixcloud() {
   # script without parameters should give usage info
   local output_file
-  output_file="$("$root_script" --OUT_DIR ../temp --SPLITTER "" get "https://www.mixcloud.com/djsupermarkt_tooslowtd/minimix-for-tstd-edits-10-those-guys-from-athens-limited-colored-double-7/")"
+  output_file="$("$root_script" --FORMAT wav --OUT_DIR ../temp --SPLITTER "" get "https://www.mixcloud.com/djsupermarkt_tooslowtd/minimix-for-tstd-edits-10-those-guys-from-athens-limited-colored-double-7/")"
   assert_equals "../temp/Minimix_for_TSTD_EDITS_10_-_Those_Guys_From_Athens_Limited_colored_Double_7.394s.wav" "$output_file"
   assert_equals 69513294 "$(< "$output_file" wc -c | awk '{print $1}')"
 }
@@ -49,7 +49,7 @@ test_download_mixcloud() {
 test_download_apple_podcast() {
   # script without parameters should give usage info
   local output_file
-  output_file="$("$root_script" --OUT_DIR ../temp --SPLITTER "" get "https://podcasts.apple.com/us/podcast/hearing-aids-by-roddy-doyle/id1281767970?i=1000632032467")"
+  output_file="$("$root_script" --FORMAT wav --OUT_DIR ../temp --SPLITTER "" get "https://podcasts.apple.com/us/podcast/hearing-aids-by-roddy-doyle/id1281767970?i=1000632032467")"
   assert_equals "../temp/Hearing_Aids_by_Roddy_Doyle.852s.wav" "$output_file"
   assert_equals 150460628 "$(< "$output_file" wc -c | awk '{print $1}')"
 }
